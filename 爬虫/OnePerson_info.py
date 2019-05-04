03f007af39edce402d6d9f516bcafe214e568d63
Greenbirch2007 <291109028@qq.com>
2019-05-04 11:19:36 +0800

    单一页面解析完毕，明天整理爬取吧

---
+++ 爬虫/OnePerson_info.py
@@ -0,0 +1,117 @@
+#! -# ! -*- coding:utf-8 -*-
+
+import time
+import re
+import urllib
+
+import pymysql
+import requests
+from selenium import webdriver
+# 还是要用PhantomJS
+import datetime
+import string
+from lxml import etree
+import random
+
+
+
+def call_pages(url):
+    driver.get(url)
+    time.sleep(3)
+    html = driver.page_source
+
+    return html
+
+
+def parse_pages(html):
+    big_list = []
+    selector = etree.HTML(html)
+    name = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[1]/div[1]/h2/div/text()')
+    age_location = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[2]/span/text()')
+    couple_g = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[1]/div[2]/div[2]/div/div/text()')
+    education = selector.xpath('//*[@id="mainContent"]/section/article/section[2]/div/ul/li[2]/div[1]/div/span/text()')
+    high = selector.xpath('//*[@id="mainContent"]/section/article/section[2]/div/ul/li[8]/div[1]/div/span/text()')
+    rearch_for = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[3]/span/span/text()')
+    pictures = selector.xpath('//*[@id="mainContent"]/section/article/section[3]/div/div/div/div/div/img/@src')
+    for item in pictures:
+        downPic = item[:-11]
+        picDown.append(downPic)
+
+
+
+    for i1,i2,i3,i4,i5,i6 in zip(name,age_location,couple_g,education,high,rearch_for):
+        big_list.append((i1,i2,i3,i4,i5,i6))
+
+    return big_list
+
+
+
+def insertDB(content):
+    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='MatchJ_love',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
+    # 这里是判断big_list的长度，不是content字符的长度
+    try:
+        cursor.executemany('insert into MatchLove_OnePersonInfo (name,age_location,couple_g,education,high,rearch_for) values (%s,%s,%s,%s,%s,%s)', content)
+        connection.commit()
+        connection.close()
+        print('向MySQL中添加数据成功！')
+    except Exception :
+        print('出列啦')
+
+
+
+
+
+
+# 先登录，再遍历访问路由
+if __name__ == '__main__':
+    picDown = []
+
+    url = 'https://jp.match.com/login/'  # 直接到登录界面！
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    driver.find_element_by_xpath('//*[@id="email"]').send_keys("291109028@qq.com")  # 用户名
+    driver.find_element_by_xpath('//*[@id="password"]').send_keys("mingyifan2007")  # 密码
+    # 点击登录
+    driver.find_element_by_xpath('//*[@id="mainContent"]/section/form/div/div/div[1]/div[5]/button').click()
+    time.sleep(1)
+
+
+
+
+    url_Person ='https://jp.match.com/profile/biOCflpwSLPBsUsUGsQjPQ2?page=5&searchType=oneWaySearch&sortBy=1'
+    html = call_pages(url_Person)
+    content = parse_pages(html)
+    insertDB(content)
+
+    for item in picDown:
+        url_name = url_Person[29:52] + random.choice(url_Person)
+
+        urllib.request.urlretrieve(item, '/home/g/Documents/matchLove_Pics/%s.jpg' % url_name )
+
+    print(content)
+
+
+
+
+
+
+
+
+
+# create table MatchLove_OnePersonInfo (
+# id int not null primary key auto_increment,
+# name varchar(30),
+# age_location varchar(60),
+# couple_g varchar(30),
+# education varchar(30),
+# high varchar(30),
+# rearch_for text
+# ) engine =InnoDB charset=utf8;
+
+# drop table ALlText_words_link1;
+#
+
+
