4449124be0f51938614d69bf321e1f7fecc3c34f
Greenbirch2007 <291109028@qq.com>
2019-05-05 16:59:12 +0800

    将数据处理和图片下载分开两个脚本

--- 爬虫/DownPics.py
+++ 爬虫/DownPics.py
@@ -22,23 +22,9 @@ def call_pages(url):
 
     return html
 
+# 同一个脚本，把数据和下载图片分开处理
+#　优化，做一个判断，如果没有图片，就直接break! 不能傻傻的等着！这样太没有效率了！
 
-def parse_pages(html):
-    big_list = []
-    selector = etree.HTML(html)
-    name = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[1]/div[1]/h2/div/text()')
-    age_location = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[2]/span/text()')
-    couple_g = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[1]/div[2]/div[2]/div/div/text()')
-    education = selector.xpath('//*[@id="mainContent"]/section/article/section[2]/div/ul/li[2]/div[1]/div/span/text()')
-    high = selector.xpath('//*[@id="mainContent"]/section/article/section[2]/div/ul/li[8]/div[1]/div/span/text()')
-    rearch_for = selector.xpath('//*[@id="mainContent"]/section/article/div[2]/div[2]/div[3]/span/span/text()')
-
-
-
-    for i1,i2,i3,i4,i5,i6 in zip(name,age_location,couple_g,education,high,rearch_for):
-        big_list.append((i1,i2,i3,i4,i5,i6))
-
-    return big_list
 
 
 def Python_sel_Mysql():
@@ -60,21 +46,6 @@ def Python_sel_Mysql():
 
 
 
-def insertDB(content):
-    connection = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='MatchJ_love',
-                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-    cursor = connection.cursor()
-    # 这里是判断big_list的长度，不是content字符的长度
-    try:
-        cursor.executemany('insert into MatchLove_OnePersonInfo (name,age_location,couple_g,education,high,rearch_for) values (%s,%s,%s,%s,%s,%s)', content)
-        connection.commit()
-        connection.close()
-        print('向MySQL中添加数据成功！')
-    except Exception :
-        print('出列啦')
-
-
-
 
 
 
@@ -83,7 +54,7 @@ if __name__ == '__main__':
     picDown = []
 
     url = 'https://jp.match.com/login/'  # 直接到登录界面！
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get(url)
 
     driver.find_element_by_xpath('//*[@id="email"]').send_keys("291109028@qq.com")  # 用户名
@@ -95,10 +66,26 @@ if __name__ == '__main__':
 
     for url_str in Python_sel_Mysql():
         url_Person = url_str
-        html = call_pages(url_Person)
-        content = parse_pages(html)
-        insertDB(content)
+        html = call_pages(url_str)
+        selector = etree.HTML(html)
 
+        pictures = selector.xpath('//*[@id="mainContent"]/section/article/section[3]/div/div/div/div/div/img/@src')
+        if len(pictures) == 0:
+            print("本页面没有图片～～～～")
+            continue  # 必须放在一个循环体中！所以稍微改动一些
+            # break是跳过所有循环，continue是跳过本次循环
+
+        else:
+
+            for item in pictures:
+                downPic = item[:-11]
+                picDown.append(downPic)
+
+
+        for item in picDown:
+            url_name = url_Person[29:52] + random.choice(url_Person)
+
+            urllib.request.urlretrieve(item, '/home/g/Documents/matchLove_Pics/%s.jpg' % url_name )
 
         print(datetime.datetime.now())
 
@@ -109,7 +96,7 @@ if __name__ == '__main__':
 
 
 
-
+#
 # create table MatchLove_OnePersonInfo (
 # id int not null primary key auto_increment,
 # name varchar(30),
